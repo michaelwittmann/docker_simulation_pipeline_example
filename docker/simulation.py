505be7a7d15484afa01acea3f9d0248958139810
Michael Wittmann <michael.wittmann@tum.de>
2020-11-23 15:26:07 +0100

    cli for monte_carlo_pi.py

--- docker/simulation.py
+++
@@ -1,8 +0,0 @@
-from docker.docker_sim_manager import DockerManager
-from pathlib import Path
-
-if __name__ == '__main__':
-
-    sim_path = Path.home()
-    docker_manager = DockerManager(4, 100,sim_path)
-    docker_manager.start_computation()
--- simulation_monte_carlo_pi/test_monte_carlo_pi.py
+++ simulation_monte_carlo_pi/test_monte_carlo_pi.py
@@ -1,11 +1,13 @@
+from pathlib import Path
 from unittest import TestCase
 import math
 from simulation_monte_carlo_pi.monte_carlo_pi import MonteCarloPi
+
 class TestMonteCarloPi(TestCase):
 
 
     def test_estimate_pi(self):
-        simulation = MonteCarloPi(iterations=100000, random_seed=12345)
+        simulation = MonteCarloPi(iterations=100000, random_seed=12345, output_dir=Path('ouput'))
         pi_hat = simulation.estimate_pi()
         print(abs(pi_hat/math.pi -1) )
         self.assertTrue(abs(pi_hat/math.pi -1) < 0.001)
@@ -13,7 +15,7 @@ class TestMonteCarloPi(TestCase):
 
 
     def test_plot(self):
-        simulation = MonteCarloPi(iterations=100000, random_seed=12345)
+        simulation = MonteCarloPi(iterations=100000, random_seed=12345, output_dir=Path('ouput'))
         simulation.estimate_pi()
         simulation.plot()
 
