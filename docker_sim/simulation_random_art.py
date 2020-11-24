97f6bb24f08391cf5eb2fbe0e669053c002e4287
Michael Wittmann <michael.wittmann@tum.de>
2020-11-24 15:51:34 +0100

    bug fix

--- docker_sim/simulation_random_art.py
+++ docker_sim/simulation_random_art.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
                                       1,
                                       sim_path)
     for i in range(1,10):
-        docker_manager.add_sim_job(SimJob(f'size{i}x{i}',None, command=f'-g {i} -i 30 -s 5000 -o arts -n 50'))
+        docker_manager.add_sim_job(SimJob(f'size{i}x{i}',None, command=f'-g {i} -i 30 -s 5000 -o /mnt/data -n 50'))
 
     for i in range(1,10):
-        docker_manager.add_sim_job(SimJob(f'invaders_{i}',None, command=f'-g 10 -i {i} -s 5000 -o arts -n 50'))
+        docker_manager.add_sim_job(SimJob(f'invaders_{i}',None, command=f'-g 10 -i {i} -s 5000 -o /mnt/data -n 50'))
 
     docker_manager.start_computation()
